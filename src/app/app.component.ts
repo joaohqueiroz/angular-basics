62a486ab8c7b5cf5f38698fae8b9b5e415931b9a
joaohqueiroz <johnrick.jh@gmail.com>
2023-11-11 18:57:44 -0300

    Understanding Angular components relationship

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-title></app-title>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
-  title = 'meu-primeiro-projeto';
+
 }
