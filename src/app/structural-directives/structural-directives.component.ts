86bad68577c71c84e0bc5188f6878947e8b5a0fa
joaohqueiroz <johnrick.jh@gmail.com>
2023-11-12 18:27:38 -0300

    *ngIf lessons

---
+++ src/app/structural-directives/structural-directives.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-directives',
+  templateUrl: './structural-directives.component.html',
+  styleUrl: './structural-directives.component.scss',
+})
+export class StructuralDirectivesComponent  {
+  public condition: boolean = true;
+
+  public switchCondition = (): boolean => this.condition = !this.condition;
+}
