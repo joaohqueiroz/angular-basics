3cd1c1ac81cd7a1a05097b2204ea7bd7c32eca1b
joaohqueiroz <johnrick.jh@gmail.com>
2023-11-12 18:40:39 -0300

    *ngFor lessons

--- src/app/structural-directives/structural-directives.component.ts
+++ src/app/structural-directives/structural-directives.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './structural-directives.component.html',
   styleUrl: './structural-directives.component.scss',
 })
-export class StructuralDirectivesComponent  {
+export class StructuralDirectivesComponent {
+  public nome: string = '';
   public condition: boolean = true;
+  public list: Array<{ nome: string }> = [
+    { nome: 'João' },
+    { nome: 'Aldair' },
+    { nome: 'Ceza' },
+    { nome: 'Diana' },
+  ];
 
-  public switchCondition = (): boolean => this.condition = !this.condition;
+  public switchCondition = (): boolean => (this.condition = !this.condition);
+
+  public add = () => {
+    this.list.push({ nome: this.nome });
+    this.nome = '';
+  };
 }
