a989251f635c81393f72deac06a04c28c6bf42ef
joaohqueiroz <johnrick.jh@gmail.com>
2023-11-14 21:09:20 -0300

    @Input()

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponentComponent } from './header-component/header-component.component';
-
-
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponentComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponentComponent
-  ]
+  declarations: [HeaderComponentComponent, InputComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponentComponent, InputComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
